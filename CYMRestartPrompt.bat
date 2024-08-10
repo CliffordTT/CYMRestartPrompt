07d63d2212963f71e5da249cc88a10ae53ac0d62
CliffordTT <148488207+CliffordTT@users.noreply.github.com>
2024-08-10 18:43:56 +0200

    Update CYMRestartPrompt.bat

--- CYMRestartPrompt.bat
+++ CYMRestartPrompt.bat
@@ -1,4 +1,19 @@
 @echo off
+color 02
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
+::: Dieses Script wurde erstellt von:
+:::   _____ __     __ __  __   ____    _____    _____  _______
+:::  / ____|\ \   / /|  \/  | / __ \  / ____|  |_   _||__   __|
+::: | |      \ \_/ / | \  / || |  | || (___      | |     | |   
+::: | |       \   /  | |\/| || |  | | \___ \     | |     | |   
+::: | |____    | |   | |  | || |__| | ____) |_  _| |_    | |   
+:::  \_____|   |_|   |_|  |_| \____/ |_____/(_)|_____|   |_|   
+echo.
+echo Ersteller: TT
+echo Erstellungsdatum: 10.08.2024
+echo Version 1.0
+echo.
+
 setlocal
 echo Computer neu starten? (J/N)
 set /p userChoice=
