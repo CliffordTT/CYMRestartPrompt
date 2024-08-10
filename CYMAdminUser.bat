0a59659df6fbcecd04703b9a991d06118201adf4
CliffordTT <148488207+CliffordTT@users.noreply.github.com>
2024-08-10 19:30:33 +0200

    Update CYMAdminUser.bat

--- CYMAdminUser.bat
+++ CYMAdminUser.bat
@@ -14,6 +14,44 @@ echo Erstellungsdatum: 10.08.2024
 echo Version 1.0
 echo.
 
+echo C:\_CYMOS wird erstellt
+echo.
+mkdir C:\_CYMOS
+echo _CYMOS Folder erstellt
+echo.
+
+echo Enrgieoptionen werden angepasst
+echo.
+powercfg /change monitor-timeout-ac 0
+powercfg /change monitor-timeout-dc 0
+powercfg /change standby-timeout-ac 0
+powercfg /change standby-timeout-dc 0
+powercfg /change disk-timeout-ac 0
+powercfg /change disk-timeout-dc 0
+for /f "tokens=*" %%A in ('"wmic path Win32_NetworkAdapter where (NetEnabled='TRUE') get DeviceID"') do (
+    if not "%%A"=="DeviceID" (
+        REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}\%%A" /v PnPCapabilities /t REG_DWORD /d 24 /f
+    )
+)
+echo.
+echo Energieoptionen wurden angepasst
+echo.
+
+echo Anpassen der OEM-Informationen
+echo.
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v Manufacturer /t REG_SZ /d "CYMOS Medizin IT" /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v SupportURL /t REG_SZ /d "https://www.cymos.de" /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v SupportPhone /t REG_SZ /d "06039/709000" /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v SupportHours /t REG_SZ /d "Mo.-Fr. 08-17 Uhr" /f
+mkdir C:\Windows\OEM
+echo .
+curl https://www.cymos.de/wp-content/uploads/2022/04/CYMOS-Logo-medizin-it.png -o C:\Windows\OEM\CYMLogo01.bmp
+echo .
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v Logo /t REG_SZ /d "C:\Windows\OEM\CYMLogo01.bmp" /f
+echo.
+echo OEM-Informationen erfolgreich aktualisiert!
+echo.
+
 echo Darkmode wird aktiviert
 echo.
 REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v AppsUseLightTheme /t REG_DWORD /d 0 /f
@@ -22,7 +60,7 @@ echo.
 echo Darkmode aktiviert
 echo.
 
-eecho Taskleiste wird angepasst
+echo Taskleiste wird angepasst
 echo.
 REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Search" /v SearchboxTaskbarMode /t REG_DWORD /d 1 /f
 REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarAl /t REG_DWORD /d 0 /f
@@ -30,6 +68,22 @@ REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Ad
 REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v ShowTaskViewButton /t REG_DWORD /d 0 /f
 echo.
 echo Taskleiste wurde angepasst
+echo.
+
+echo Standard Software Instalation wird ausgeführt
+echo.
+curl https://ninite.com/7zip-chrome-firefox-flash-foxit-javaoraclex8-teamviewer-vlc/ninite.exe -o C:\_CYMOS\CYMNinite01.exe && C:\_CYMOS\CYMNinite01.exe
+echo.
+echo Instllation abgeschlossen
+echo.
+
+echo Entfernen von Edge, Foxit Reader & VLC vom Öffentlichen Desktop
+echo.
+del "C:\Users\Public\Desktop\Microsoft Edge.lnk"
+del "C:\Users\Public\Desktop\Foxit PDF Reader.lnk"
+del "C:\Users\Public\Desktop\VLC media player.lnk"
+echo.
+echo Verknüpfungen wurden vom Öffentlichen Desktop entfernt
 echo.
 
 echo Alle Taskleisten-Icons werden entfernt
@@ -41,17 +95,6 @@ REG DELETE "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer
 echo Alle Taskleisten-Icons wurden entfernt
 echo.
 
-echo Entfernen von Edge vom User Desktop
-echo.
-setlocal
-set "currentUser=%USERNAME%"
-set "desktopPath=%USERPROFILE%\Desktop"
-del "%desktopPath%\Microsoft Edge.lnk"
-endlocal
-echo.
-echo Verknüpfungs wurden vom User Desktop entfernt
-echo.
-
 echo Explorer wird neugestartet
 echo.
 taskkill /f /im explorer.exe
@@ -60,9 +103,45 @@ echo.
 echo Explorer wurde neugestartet
 echo.
 
-msg * "Userskiript wurde durch geführt. Bitte Logs prüfen!"
+echo Debloaten von Windows
+echo.
+sc config WaaSMedicSvc start= disabled
+sc config WSearch start= disabled
+sc config XblAuthManager start= disabled
+sc config XblGameSave start= disabled
+sc config XboxNetApiSvc start= disabled
+sc config DiagTrack start= disabled
+sc stop DiagTrack
+sc config dmwappushsvc start= disabled
+sc stop dmwappushsvc
+echo Lösche temporäre Dateien und Caches...
+del /q /f /s %TEMP%\*
+del /q /f /s C:\Windows\Temp\*
+echo Deaktiviere Telemetrie und Datensammlung...
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v "AllowTelemetry" /t REG_DWORD /d "0" /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v "AllowCortana" /t REG_DWORD /d "0" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "OneDrive" /t REG_SZ /d "" /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows\Windows Error Reporting" /v "Disabled" /t REG_DWORD /d "1" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Remote Assistance" /v "fAllowToGetHelp" /t REG_DWORD /d "0" /f
+curl https://raw.githubusercontent.com/Raphire/Win11Debloat/master/Get.ps1 -o C:\_CYMOS\CYMWinDebloat01.ps1 && powershell -ExecutionPolicy Bypass -File "C:\_CYMOS\CYMWinDebloat01.ps1 -RemoveApps"
+echo Windows wurde Debloatet
+echo.
+
+echo PC-Namen/ Domain Join durchführen
+sysdm.cpl
+
+echo.
+start C:\Windows\Setup
+echo.
+
+msg * "Adminskiript wurde durch geführt. Bitte Logs prüfen!"
 
 echo  Neustart Prompt
 curl https://raw.githubusercontent.com/CliffordTT/CYMScripts/main/CYMRestartPrompt.bat -o C:\_CYMOS\CYMRestartPrompt.bat && start C:\_CYMOS\CYMRestartPrompt.bat
 
 pause
+
+::setlocal
+::set "PC_NAME=%COMPUTERNAME%"
+::msg * Der Computername ist: %PC_NAME%
+::endlocal
